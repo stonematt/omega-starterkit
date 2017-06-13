daa231ab295ed2f34c8a3cead1ec757e66981f26
stone <mstone@stonecover.com>
2017-06-13 11:02:21 -0600

    code cleanup

--- temp/STK08-temp-sensor.py
+++ temp/STK08-temp-sensor.py
@@ -7,7 +7,6 @@ import oneWire
 
 # setup onewire and polling interval
 oneWireGpio = 19 # set the sensor GPIO
-#pollingInterval = 1 # seconds
 
 # use command line argument to set the polling interval
 print len(sys.argv)
@@ -36,7 +35,6 @@ def __main__():
     while 1:
         # check and print the temperature
         value = sensor.readValue()
-        # print "T = " + str(value) + " C" # replaced for output with timestamp
         print str(datetime.now()) + " , " + str(value) + " C"
         time.sleep(pollingInterval)
 
